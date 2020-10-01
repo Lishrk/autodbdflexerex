14409ea985be71dcb66cc8661fab02465ad60bea
TrickyBestia <thewonderfulcookie@gmail.com>
2020-10-01 16:06:37 +0300

    Linked Costura.Fody

--- AutoDbdFlexerEx/App.xaml.cs
+++ AutoDbdFlexerEx/App.xaml.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Reflection;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AutoDbdFlexerEx
 {
     public partial class App : Application
     {
-        private void OnStartup(object s, StartupEventArgs e)
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += (s1, e1) =>
-            {
-                if (e1.Name.Substring(0, e1.Name.IndexOf(",")) == "Newtonsoft.Json")
-                {
-                    return Assembly.Load(AutoDbdFlexerEx.Properties.Resources.Newtonsoft_Json);
-                }
-                return null;
-            };
-        }
+
     }
 }
